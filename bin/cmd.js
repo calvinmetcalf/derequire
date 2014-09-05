93dc0fa64810f55ca25937b7ace262010e8eceba
Calvin Metcalf <cmetcalf@appgeo.com>
2014-09-05 10:44:58 -0400

    add more command line options

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,13 +3,30 @@
 var derequire = require('../');
 var concat = require('concat-stream');
 var fs = require('fs');
-
-var file = process.argv[2];
-var input = file && file !== '-'
-    ? fs.createReadStream(process.argv[2])
-    : process.stdin
-;
+var argv = require('yargs')
+  .options('t', {
+      alias : 'to',
+      default : '_dereq_',
+      describe: 'token to change the variable into'
+  })
+   .options('f', {
+      alias : 'from',
+      default : 'require',
+      describe: 'token to find and change'
+  })
+   .help('h')
+   .alias('h', 'help')
+   .version(require('../package.json').version, 'v')
+   .alias('v', 'version')
+   .argv;
+var file = argv._[0];
+var input;
+if (file && file !== '-') {
+  input = fs.createReadStream(file);
+} else {
+  input = process.stdin;
+} 
 
 input.pipe(concat(function(buf) {
-    console.log(derequire(buf.toString('utf8')));
+    console.log(derequire(buf.toString('utf8'), argv.t, argv.f));
 }));
